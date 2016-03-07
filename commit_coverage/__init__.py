8233777d0d86af01d7c6d792ea5e2ac14284f123
Paul Michali <pc@michali.net>
2016-03-07 14:12:22 -0500

    Getting install working

--- commit_coverage/__init__.py
+++ commit_coverage/__init__.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
+#
+# Copyright (c) 2016 Paul Michali, Cisco Systems Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
 # commit_coverage
 #
 # Indicates the coverage for diffs specified in the command. It assumes that
@@ -59,6 +75,8 @@ diff_region_re = re.compile(r'@@\s[-]\S+\s[+](\S+)\s@@')
 source_line_re = re.compile(r'<p id="n(\d+)" class="([^"]+)"')
 title_re = re.compile(r'\s*<title>Coverage for [^:]+:\s+(\d+%)<\/title>')
 summary_end_re = re.compile(r'\s+<td class="text">')
+
+__version__ = "0.1"
 
 
 class DiffCollectionFailed(Exception):
